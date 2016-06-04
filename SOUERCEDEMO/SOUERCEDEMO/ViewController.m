2da1a26302554b5f8ddefeff92dbc011b35bbb79
lilongcnchome <lilongcnc@vip.qq.com>
2016-06-04 22:54:17 +0800

    2

---
+++ SOUERCEDEMO/SOUERCEDEMO/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  SOUERCEDEMO
+//
+//  Created by 李龙 on 16/6/4.
+//  Copyright © 2016年 lauren. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"1");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
